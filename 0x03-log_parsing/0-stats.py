accfe3f4c14f64354c8202dc9c040e0028a2091c
funminiyiop <funminiyiop@gmail.com>
2024-06-20 18:42:56 +0100

    correction

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,37 +1,38 @@
 #!/usr/bin/python3
-'''a script that reads stdin line by line and computes metrics'''
-
+"""
+Log parsing with metrics computation
+"""
 
 import sys
+import re
 
-cache = {'200': 0, '301': 0, '400': 0, '401': 0,
-         '403': 0, '404': 0, '405': 0, '500': 0}
-total_size = 0
-counter = 0
+if __name__ == '__main__':
 
-try:
-    for line in sys.stdin:
-        line_list = line.split(" ")
-        if len(line_list) > 4:
-            code = line_list[-2]
-            size = int(line_list[-1])
-            if code in cache.keys():
-                cache[code] += 1
-            total_size += size
-            counter += 1
+    line_count = 0
+    total_file_size = 0
+    status_codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    stats = {code: 0 for code in status_codes}
 
-        if counter == 10:
-            counter = 0
-            print('File size: {}'.format(total_size))
-            for key, value in sorted(cache.items()):
-                if value != 0:
-                    print('{}: {}'.format(key, value))
+    def print_stats(stats: dict, total_size: int) -> None:
+        print("File size:", total_size)
+        for code in sorted(stats):
+            if stats[code] > 0:
+                print(code + ":", stats[code])
 
-except Exception as err:
-    pass
-
-finally:
-    print('File size: {}'.format(total_size))
-    for key, value in sorted(cache.items()):
-        if value != 0:
-            print('{}: {}'.format(key, value))
+    try:
+        for line in sys.stdin:
+            line_count += 1
+            line = line.strip()
+            match = re.match(r'.*GET \/projects\/260 HTTP\/1\.1" (\d+) (\d+)', line)
+            if match:
+                status_code = match.group(1)
+                file_size = int(match.group(2))
+                if status_code in status_codes:
+                    stats[status_code] += 1
+                total_file_size += file_size
+            if line_count % 10 == 0:
+                print_stats(stats, total_file_size)
+        print_stats(stats, total_file_size)
+    except KeyboardInterrupt:
+        print_stats(stats, total_file_size)
+        raise
