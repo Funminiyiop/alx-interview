3f5a811677f6e2b864faa108a42080148d97f6c3
funminiyiop <funminiyiop@gmail.com>
2024-07-12 03:08:20 +0100

    adding corrected task file for 0x06-starwars_api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,25 +1,41 @@
 #!/usr/bin/node
+
 const request = require('request');
-const API_URL = 'https://swapi-api.hbtn.io/api';
+const movieId = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
 
-if (process.argv.length > 2) {
-  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
-    if (err) {
-      console.log(err);
+async function getCharacterName (characters, index, names = []) {
+  if (index === characters.length) {
+    names.forEach((name) => console.log(name));
+    return;
+  }
+  const characterPath = characters[index];
+  request(characterPath, (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else if (response.statusCode !== 200) {
+      console.error('Unexpected status code:', response.statusCode);
+    } else {
+      // console.log(JSON.parse(body).name);
+      names.push(JSON.parse(body).name);
+      getCharacterName(characters, index + 1, names);
     }
-    const charactersURL = JSON.parse(body).characters;
-    const charactersName = charactersURL.map(
-      url => new Promise((resolve, reject) => {
-        request(url, (promiseErr, __, charactersReqBody) => {
-          if (promiseErr) {
-            reject(promiseErr);
-          }
-          resolve(JSON.parse(charactersReqBody).name);
-        });
-      }));
-
-    Promise.all(charactersName)
-      .then(names => console.log(names.join('\n')))
-      .catch(allErr => console.log(allErr));
   });
 }
+if (process.argv.length !== 3) {
+  console.log('Usage: ./0-starwars_characters.js <filmId>');
+  process.exit(1);
+}
+async function getMovies () {
+  request(url, (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else if (response.statusCode !== 200) {
+      console.error('Unexpected status code:', response.statusCode);
+    } else {
+      const characters = JSON.parse(body).characters;
+      getCharacterName(characters, 0);
+    }
+  });
+}
+getMovies();
