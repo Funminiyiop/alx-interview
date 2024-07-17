0ba44eca8828a528ce34f3fdc99d99147977f25e
funminiyiop <funminiyiop@gmail.com>
2024-07-17 16:07:46 +0100

    corrected rotate_2d_matrix project file

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -7,12 +7,11 @@ def rotate_2d_matrix(matrix):
     """Rotates an m by n 2D matrix in place.
     """
 
-    for i in range(int(len(matrix) / 2)):
-        for j in range(i, (len(matrix) - i - 1)):
-            x = (len(matrix) - 1 - j)
-            tmp = matrix[i][j]
-            matrix[i][j] = matrix[x][i]
-            matrix[x][i] = matrix[(len(matrix) - i - 1)][x]
-            matrix[(len(matrix) - i - 1)][x] = matrix[j][(len(matrix) - i - 1)]
-            matrix[j][(len(matrix) - i - 1)] = tmp
-
+    n = len(matrix)
+    # Transpose the matrix
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    # Reverse each row
+    for i in range(n):
+        matrix[i].reverse()
