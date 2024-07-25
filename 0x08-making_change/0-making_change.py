51d1725eb42442b36af9124cb4d2ce1c7237758e
funminiyiop <funminiyiop@gmail.com>
2024-07-25 12:11:34 +0100

    correcting making change file

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 """
-Given a pile of coins of different values, determine the fewest number of coins
-needed to meet a given amount total.
+This is to determine the fewest
+number of coins needed to meet a given amount total.
+
+Return: fewest number of coins needed to meet total
+If total is 0 or less, return 0
+If total cannot be met by any number of coins you have, return -1
+
+Hint:
+coins is a list of the values of the coins in your possession
+The value of a coin will always be an integer greater than 0
+assume an infinite number of each denomination of coin in the list
 """
 
 
@@ -12,7 +21,7 @@ def makeChange(coins, total):
     If total is 0 or less, return 0.
     If total cannot be met by any number of coins you have, return -1.
     """
-    if total < 1:
+    if total <= 0:
         return 0
 
     dp = [float('inf')] * (total + 1)
