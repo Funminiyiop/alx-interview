38e88baed09b0db6bf1794ff8ec014777297b290
funminiyiop <funminiyiop@gmail.com>
2024-07-25 12:52:25 +0100

    the corrected making_change file

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -25,18 +25,14 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
+    number_coins = 0
     coins.sort(reverse=True)
 
-    i, ncoins = (0, 0)
-    cpy_total = total
-    len_coins = len(coins)
+    for coin in coins:
+        while coin <= total:
+            total -= coin
+            number_coins += 1
 
-    while(i < len_coins and cpy_total > 0):
-        if (cpy_total - coins[i]) >= 0:
-            cpy_total -= coins[i]
-            ncoins += 1
-        else:
-            i += 1
-
-    check = cpy_total > 0 and ncoins > 0
-    return -1 if check or ncoins == 0 else ncoins
+    if total == 0:
+        return number_coins
+    return -1
