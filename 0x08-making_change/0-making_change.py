cfb5af4d8b8eaa6dbdaa57953cd9fd04b7eb24e8
funminiyiop <funminiyiop@gmail.com>
2024-07-25 12:30:27 +0100

    corrected making_change file

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-This is to determine the fewest
-number of coins needed to meet a given amount total.
+This is to determine the fewest number of coins
+needed to meet a given amount total.
 
 Return: fewest number of coins needed to meet total
 If total is 0 or less, return 0
@@ -21,17 +21,15 @@ def makeChange(coins, total):
     If total is 0 or less, return 0.
     If total cannot be met by any number of coins you have, return -1.
     """
+    
+    if total <= 0:
+        return 0
 
-    if not coins or coins is None:
-            return -1
-        if total <= 0:
-            return 0
-        change = 0
-        coins = sorted(coins)[::-1]
-        for coin in coins:
-            while coin <= total:
-                total -= coin
-                change += 1
-            if (total == 0):
-                return change
-        return -1
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
